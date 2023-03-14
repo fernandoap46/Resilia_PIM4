ac4327fb765f4543f14fc2ff0089ffe64312bacd
fernandoap46 <fernandoap46@gmail.com>
2023-03-14 08:41:03 -0300

    Comentarios codigo classe Biblioteca de Midia Detalhamento metodo tocar playlist

--- src/Model/BibliotecaDeMidias.java
+++ src/Model/BibliotecaDeMidias.java
@@ -3,12 +3,13 @@ package Model;
 import com.sun.source.tree.ReturnTree;
 
 import java.util.ArrayList;
-
+// -----------------------------------Atributo Classe Biblioteca de Midias-------------------------------- //
 public class BibliotecaDeMidias {
     private ArrayList<Midia> midias;
     private Usuario usuario;
     private Playlist playlist;
 
+    // -----------------------------------Constructor-------------------------------------------------- //
     public BibliotecaDeMidias(ArrayList<Midia> midias, Usuario usuario) {
         this.midias = midias;
         this.usuario = usuario;
@@ -47,6 +48,8 @@ public class BibliotecaDeMidias {
     public void setPlaylist(Playlist playlist) {
         this.playlist = playlist;
     }
+
+    //----------------------------------Metodo tocar play list-----------------------------//
     public void tocar_playlist_music(){
 
 
@@ -54,12 +57,13 @@ public class BibliotecaDeMidias {
         System.out.println(midias.size());
 
         System.out.println("\n  Minha playlist musica  \n");
+    //----------utilizando metodo que inicia na posição 0 e percorre osdados que são < que 0 //
         for (int i = 0; i < midias.size(); i++) {
             System.out.println("playlist: " + midias.get(i));
 
         }
         System.out.println("Musica tocando");
-
+//-------------------Retorna posição atual------------------------------------------------- //
         System.out.println(this.playlist.getMidia_atual());
 
 
