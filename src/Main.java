111ecbb6c644d95712dab5a6b2910fe44e75cc49
fernandoap46 <fernandoap46@gmail.com>
2023-03-14 09:24:49 -0300

    Ajuste comentarios e descrião do codigo

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //-------------Menu inicial-------------------------------------------------
+
         System.out.println("\n Biblioteca de Midias \n"+"\n Digite o que deseja \n");
 
-
-
         ArrayList<Midia> midias = new ArrayList<>();
-
+        //-------------Dados mocados-------------------------------------------------
         midias.add(new Midia("Analua", "2004", "reggae"));
         midias.add(new Midia("Toca uma reggueira ai", "2004", "reggae"));
         midias.add(new Midia("Paulinha", "2004", "reggae"));
@@ -30,26 +30,31 @@ public class Main {
         int escolha=10;
         Scanner sc = new Scanner(System.in);
 
+        //-------------Interação usuario para executar metodos criados-----------------
         do {
-            System.out.println("1- Tocar minha playlist musica ");
-            System.out.println("2- Tocar proxima midia");
-            System.out.println("3- Tocar minha playlist musica");
-            System.out.println("1- Tocar minha playlist musica ");
+            System.out.println("\n1- Tocar minha playlist musica ");
+            System.out.println("2- Tocar proxima musica");
+            System.out.println("3- Tocar musica anterior");
+            System.out.println("10- Encerrar player ");
             System.out.println();
 
             escolha = sc.nextInt();
             switch (escolha){
+
+                //-------------Utilizando metodo iniciando midia atual-----------------
                 case 1: {
                     biblioteca.getPlaylist().getMidia_atual();
                     biblioteca.tocar_playlist_music();
                     break;
                 }
+                //-------------Utilizando metodo proxima musica-----------------
                 case 2:{
                     biblioteca.getPlaylist().proxima_midia();
                     biblioteca.tocar_playlist_music();
                     break;
 
                 }
+                //-------------Utilizando metodo musica anterior-----------------
                 case 3:{
                     biblioteca.getPlaylist().midia_anterior();
                     biblioteca.tocar_playlist_music();
